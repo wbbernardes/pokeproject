0896f41a4a996cedaf459e307178fb927a7133cf
Wesley Brito <wesleypkn@gmail.com>
2023-05-26 23:18:31 -0300

    First setup

---
+++ pokeProject/pokeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokeProjectApp.swift
+//  pokeProject
+//
+//  Created by Wesley Brito on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct pokeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
